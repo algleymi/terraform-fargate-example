551b77852532e6b4f92f92ed2e84d6f2d2a70662
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-03-19 15:48:16 +0100

    feat: export container name and port to be used by appspec

--- modules/fargate/outputs.tf
+++ modules/fargate/outputs.tf
@@ -1,3 +1,11 @@
 output "task_definition_arn" {
   value = aws_ecs_task_definition.task_definition.arn
 }
+
+output "container_name" {
+  value = local.container_name
+}
+
+output "container_port" {
+  value = local.container_port
+}
