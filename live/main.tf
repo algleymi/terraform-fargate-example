5f263515d171ae2d43378365ec1ed4b829087fa2
Arno Schutijzer <arno.schutijzer@protonmail.com>
2022-04-10 23:30:32 +0200

    feat: provision application task definition

--- live/main.tf
+++ live/main.tf
@@ -2,6 +2,15 @@ locals {
   identifier = "example"
 }
 
+module "application" {
+  source = "../modules/application"
+
+  identifier = local.identifier
+  # This value can be injected when running `terraform apply -var "image-tag=tag-here"`.
+  # This is especially useful when using immutable docker tags.
+  image_tag = var.image_tag
+}
+
 module "network" {
   source = "../modules/network"
 
